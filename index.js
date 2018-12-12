78499f98730f527581cdd5c3ecf86dfa224327ad
Wilker <10854855+Wilker-a@users.noreply.github.com>
2018-12-12 18:50:53 -0200

    Changed logic and a bit of a progress. i think the code should be more readable now.

--- index.js
+++ index.js
@@ -16,25 +16,51 @@
  *   //user-given parameters should be represented by '*'.
  *   //if a string is provided, the user's argument is appended,
  *   //otherwise replaced.
- *   "--file=*|-f *": ""
- * }
- * @param {array} givenParams is the array containing the parameters to be
+ *   "--file=*|-f *": "",
+ * 
+ *   //if the given parameter does not start with a dash,
+ *   //the return value should be an object. in which case,
+ *   //this function is executed recursively.
+ *   "do": {
+ *     "this": true,
+ *     "that": 1,
+ *   }
+ * }`
+ * @param {string[]} givenParams is the array containing the parameters to be
  *   compared aganist. Default is `process.argv`.
  * @returns {object} The given variables to the given arguments.
  *   The keys are identified by the first parameter.
  */
-const parseArguments = (object, givenParams = process.argv) => {
-	if(typeof object !== "object") throw new TypeError("Coudn't parse arguments. Given parser argument should be an object.")
+const parseArguments = (object, givenParams = [...process.argv.slice(2)]) => {
+	if(typeof object !== "object") throw new TypeError("Error while parsing arguments. Given parser argument should be an object.")
+	/** The returning variables to sending */
 	var active = {}
-	for(var key in object) { //for each given key/param... 
-		const args = key.split("|") //split the string into different keys with '|'
-		args.every((key,index) => { //gathers the acquired keys...
-			if(key.startsWith('--'))
-				if(key.endsWith('=*')) active[key] = givenParams[index]
-				else {}
-			else if(key.startsWith('-')) //if it starts with one minus (e.g "-b")...
-				if(key.length>2 && !key.endsWith(' *')) throw new Error('"-" parameters cannot contain more than one letter.')
-				else if(key.endsWith(" *")) active[key] = givenParams[index]
+	for(var key in object) { //for each given key:param...
+		if(key.search("||")) throw new SyntaxError('Found "||". single pipe ("|") should be used instead.')
+
+		/** the different keys split with '|' to form the strings */
+		const args = key.split("|")
+		
+		//gathers the acquired keys...
+		args.every((key,index) => {
+			active[key] =
+				//if argument is found...
+				givenParams.includes(key.slice(0,key.length-3))?
+					//if key ends with "=*"...
+					key.endsWith("=*")?
+						//(=*) splits the key, gathers the content after "=".
+						givenParams[givenParams.indexOf(key.slice(0,key.length-3))].split('=',2)[1]:false
+					//else if ends with " *"...
+					|| key.endsWith(" *")?
+						//(*) gathers the content after the parameter.
+						givenParams[givenParams.indexOf(key.slice(0,key.length-3))]:false
+					||//otherwise...
+					object[key] //just the value of the object key.
+				//if argument is not found, null.
+				:null;
+
+			//if active is null and 'null' in the object represents the active argument, continue the loop.
+			if(active[key] === null && object[key] === null) return true
 		})
 	}
 	return active
