b8e12e7c26175bc11dc72aa84d48cc864f1e29d6
Wilker <10854855+Wilker-a@users.noreply.github.com>
2018-12-17 18:57:44 -0200

    New branch with another logic to see if it works better.

--- index.js
+++ index.js
@@ -9,9 +9,75 @@ class ParamNotFound extends Error {
 	 *   "message" and "stack"
 	 */
 	constructor(...args) {
-        super(...args)
-        Error.captureStackTrace(this, ParamNotFound)
-    }
+		super(...args)
+		Error.captureStackTrace(this, ParamNotFound)
+	}
+}
+
+/**
+ * @description gathers a string and returns its validation
+ * @param {string} match is the string to be checked 
+ * @param {number} index (for iteration) the index of the current array iteration
+ * @param {Array} array (for iteration) 
+ * @returns {boolean} a boolean determining weither or not this argument
+ *   asks for another parameter
+ */
+function checkSyntax(match,index,array) {
+	if (typeof match!=="string") throw new TypeError("Invalid type for Match validation at the given string! \""+match+"\"")
+	var type
+	if(//if(none of the sintaxes match)...
+		(type =
+			//letters, numbers and dashes, should start with 2 dashes ("--"),
+			//  should not end with a dash.
+			match.match(/^--[a-zA-Z0-9-]{1,}[a-zA-Z0-9]$/)?false:null
+			//same as before, but including next param
+			|| match.match(/^--[a-zA-Z0-9-]{1,}[a-zA-Z0-9]=<>$/)?true:null
+			//one dash and one (lower|upper)case letter.
+			|| match.match(/^-[a-zA-Z]{1}$/)?false:null
+			//same as before, but including next param
+			|| match.match(/^-[a-zA-Z]{1} <>$/)?true:null
+		)===null) throw new SyntaxError("Given parameter syntax is not valid!")
+	else return type
+}
+
+/**
+ * Class for managing matches to the arguments list. This is actually
+ * just an assistance for auto-completing and stuff. It represents
+ * a possible argument that the user can give when executing your
+ * script via terminal.
+ */
+class Match {
+	/**
+	 * @description Creates a parameter and checks its validation
+	 * @constructor aasdas
+	 * @param {string|string[]} matches the possible argument(s) that 
+	 *   the user can give for this return.
+	 * @param {*} returns the returning value if the argument is found.
+	 * @param {string} name is the label for the key when returning 
+	 *   the parsed arguments.
+	 * @param {boolean} [isRecursive=undefined] determines weither or not
+	 *   this match should be recursive. If the value is `true`, `parseArguments`
+	 *   should be executed recursively on this instance, and its returning
+	 *   value will be actually the results of `parseArguments((returns),givenParams[index...end])`.
+	 *   If the value is `false`, the `returns` will stay the same after parsing,
+	 *   regardless of its type. `undefined` should be interpreted as `true`
+	 */
+	constructor(matches,returns,name,isRecursive) {
+		if(typeof matches==="undefined") throw new TypeError("Missing match pattern.")
+		if(typeof returns==="undefined") throw new TypeError("Missing return value.")
+		if(typeof name==="undefined") throw new TypeError("Missing name.")
+		if(matches instanceof Array) {
+			this.isArgument = matches.every(checkSyntax)
+			if(matches.some(checkSyntax)!==this.isArgument)
+				throw SyntaxError("Either every or none of the parameters should ask for an aditional parameter.")
+		}
+		else this.isArgument = checkSyntax(matches)
+		this.matches = matches
+		this.returns = returns
+		this.name = name
+		this.isRecursive = isRecursive
+		if(!isRecursive&&!(returns instanceof Array || returns.every(val => val instanceof Match)
+	}
 }
 
 /**
@@ -19,90 +85,25 @@ class ParamNotFound extends Error {
  *   this function gathers each given key or set of keys and compares
  *   aganist the given node parameters by the user to parse the set
  *   equivalent values to return to the program.
- * @param {object} object is the object containing the possible arguments
- *   to execute, as well as the values to return if they are given.
- * @example
- * {
- *   //if this one-letter param is found, return true.
- *   "-b": true,
- * 
- *   //if either is found, return true.
- *   "--thisoption|--thisother": true,
- * 
- *   //if either is found, return its equivalent index.
- *   //ex: if "--thisop" is found, return "A" and ignore the rest.
- *   "--thisop|--thatop|--thirdop": ["A","B","C","??"],
- * 
- *   //user-given parameters should be represented by '*'.
- *   //if a string is provided, the user's argument is appended,
- *   //otherwise replaced.
- *   "--file=*|-f *": "",
- * 
- *   //if the given parameter does not start with a dash,
- *   //the return value should be an object. in which case,
- *   //this function is executed recursively.
- *   "do": {
- *     "this": true,
- *     "that": 1,
- *   }
- * }
- * @param {string[]} givenParams is the array containing the parameters to be
- *   compared aganist. Default is `process.argv`.
- * @returns {object} The given variables to the given arguments.
- *   The keys are identified by the first parameter.
+ * @param {Match|Match[]|Array.<{matches:(string|string[]),returns:any,name:string}>} args
+ *   is the set of arguments to be looked for.
+ * @param {string[]} givenParams is the string array containing the arguments
+ *   to be compared aganist.
+ * @note if an entry in the `args` array has an array as its `returns` value
+ *   and its `matches` value, the returning value will be the index of the first
+ *   parameter that was found.
+ * @returns {{key0,key1,key2,'...'}} the results of all parsed parameters
  */
-const parseArguments = (object, givenParams = [...process.argv.slice(2)]) => {
-	if(typeof object !== "object") throw new TypeError("Error while parsing arguments. Given parser argument should be an object.")
-	/** The returning variables to sending */
-	var active = {}
-	for(var key in object) { //for each given key:param...
-	///validating
-		if(key.includes("||")) throw new SyntaxError('Found "||". single pipe ("|") should be used instead.')
-		else if (key.startsWith("|")||key.endsWith("|")) throw new SyntaxError('A key cannot start or end with a pipe ("|"). Use the pipe to divide the arguments related to the same output.')
-
-		/** the different keys split with '|' to form the strings */
-		const args = key.split("|")
-		
-		//gathers the acquired keys...
-		args.some((key,index) => {
-			active[key] =
-				//if argument is found...
-				givenParams.some(val => val.includes(key.split(/=/,1)[0]))?
-					object[args] instanceof Array?
-						object[args][index]
-						:false
-					|| //if (endsWith "=*") {...
-					key.endsWith("=*")?
-						!key.startsWith('--')? //if it's not a "--*" key
-							new SyntaxError('Error parsing! Parameter ending with "=*" should start with two dashes ("--")'):
-							//(=*) gathers 'key=arg' at position, splits key with "=", gathers the second slice.
-							givenParams[givenParams.indexOf(key.slice(0,key.length-3))].split('=',2)[1]:false
-
-					|| //} else if (endsWith " *"){...
-					key.endsWith(" *")?
-						key.startsWith("--")? //if it's not a "-@" key
-							new SyntaxError('Error parsing! Parameter ending with " *" should start with one dash and contain only one letter ("-@")'):
-							//(*) gathers 'arg' after 'key'
-							givenParams[givenParams.indexOf(key.slice(0,key.length-3))]:false
-					|| //} else if () {
-					object[args] //just the value of the object key.
-				//if argument is not called on the program...
-				:new ParamNotFound();
-
-			//if active is null and 'null' in the object represents the active argument, continue the loop.
-			if(active[key] instanceof SyntaxError) throw active[key]
-			else if (active[key] instanceof ParamNotFound) return false
-			else if (active[key] instanceof Object)
-				active[key] = parseArguments(active[key],givenParams)
-		})
-	}
-	return active
+const parseArguments = (args, givenParams = [...process.argv.slice(2)]) => {
+	/** the results of all parsed parameters */
+	var results = {}
+	if(args instanceof Array) {
+		args.forEach((match,index) => {},results)
+	} else results[args.name] = args.returns
 }
-parseArguments({
-	hi: "oi",
-	1: "hello",
-	2: "olá",
-	4: "EEEEEEEEEEEEE"
-})
+parseArguments()
 
-module.exports = parseArguments
+module.exports = {
+	parse:parseArguments,
+	Match
+}
